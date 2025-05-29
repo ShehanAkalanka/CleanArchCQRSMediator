fb2f48aeebf08212d2a2cf318cee351f2b6c641d
Shehan Akalanka <shehan.akalanka@softlogiclife.lk>
2025-05-29 16:58:45 +0530

    add explanation comments on getBookQuery

--- CleanArchCQRSMediator.Application/Books/Queries/GetBooks/GetBookQuery.cs
+++ CleanArchCQRSMediator.Application/Books/Queries/GetBooks/GetBookQuery.cs
@@ -1,7 +1,10 @@
-﻿using MediatR;
+﻿using CleanArchCQRSMediator.Domain.Entity;
+using MediatR;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +15,9 @@ namespace CleanArchCQRSMediator.Application.Books.Queries.GetBooks
 
     //}
 
+    //Modern Record Approach:
+    //    From MediatR - defines a request that returns a response
+    //    Generic parameter<List<BookVm>> specifies the return type
+    //    This query will return a list of BookVm(Book View Models)
     public record class GetBookQuery : IRequest<List<BookVm>>;
 }
